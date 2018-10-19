8d1fdb93ae49e41591b7ce87c2aeb76014fa88fa
Cristina Tarantino <cristina@tarantino.io>
2018-10-19 16:49:31 +0100

    add algorithm mutations

---
+++ src/algorithms/mutations.js
@@ -0,0 +1,21 @@
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+  return [...arr[1]].every(c => arr[0].toLowerCase().indexOf(c.toLowerCase()) !== -1);
+}
+
+mutation(["hello", "hey"]);
+
+// TEST CASES
+// mutation(["hello", "hey"]) should return false.
+// mutation(["hello", "Hello"]) should return true.
+// mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.
+// mutation(["Mary", "Army"]) should return true.
+// mutation(["Mary", "Aarmy"]) should return true.
+// mutation(["Alien", "line"]) should return true.
+// mutation(["floor", "for"]) should return true.
+// mutation(["hello", "neo"]) should return false.
+// mutation(["voodoo", "no"]) should return false.
