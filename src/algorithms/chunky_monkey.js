92a9d2a79e5bf22f5efd0e1c153a710fdbf8b1ce
Cristina Tarantino <cristina@tarantino.io>
2018-10-19 16:45:46 +0100

    added chunky monkey algorithms

---
+++ src/algorithms/chunky_monkey.js
@@ -0,0 +1,22 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  let arr2 = [];
+
+  while(arr.length){
+    arr2.push(arr.splice(0,size));
+  }
+
+  return arr2;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+// TEST CASES
+// chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
