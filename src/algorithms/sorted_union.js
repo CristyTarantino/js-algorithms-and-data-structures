22316a0729a5f1aae2ae5ad1e30fd45ee5ed5d73
Cristina Tarantino <cristina@tarantino.io>
2019-02-12 14:51:31 +0000

    add test cases

--- src/algorithms/sorted_union.js
+++ src/algorithms/sorted_union.js
@@ -12,3 +12,9 @@ function uniteUnique(...arr) {
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// TEST CASES
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
