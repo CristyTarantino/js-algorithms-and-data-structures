3e857eb7119aca5999409d4123760a040f346b3f
Cristina Tarantino <cristina@tarantino.io>
2019-03-15 16:43:20 +0000

    algorithm drop it

---
+++ src/algorithms/drop_it.js
@@ -0,0 +1,23 @@
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+//
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+    let filteredArr = [...arr];
+
+    while(filteredArr.length > 0 && !func(filteredArr[0])) {
+        filteredArr.shift();
+    }
+
+    return filteredArr;
+}
+
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;});
+
+// TEST CASES
+// dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+// dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+// dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
+// dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+// dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
+// dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
